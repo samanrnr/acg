6fd76cc72edc04428746187948c8b957c574db15
unknown <saman.rnr@gmail.com>
2018-06-02 21:33:16 +0430

    remove dummy data

--- src/app.js
+++ src/app.js
@@ -10,12 +10,6 @@ import { addItem, removeItem, editItem } from './actions/itemsActions';
 
 const store = configureStore();
 
-const saman = store.dispatch(addContact({firstName: 'saman', lastName:'rajabnejad', contactType:'customer'}));
-const sina = store.dispatch(addContact({firstName: 'sina', lastName:'rajabnejad', contactType:'customer'}));
-// store.dispatch(editContact(saman.contact.id, {firstName: 'samane'}))
-const shirt = store.dispatch(addItem({name: 'shirt', itemType:'goods', unit:'number'}));
-store.dispatch(editItem(shirt.item.id, {name: 'jaket', saleInformation:{rate: 1000}}));
-
 
 const state = store.getState();
 console.log(state);
